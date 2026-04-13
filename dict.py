aca1a93a65dc2c52fb6138d1d10bc006508869af
Vignapriya <vigna1419@gmail.com>
2026-04-13 12:04:29 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,5 @@
+def add_key_to_dictionary(d):
+    key = raw_input("Enter key to add: ")
+    value = int(input("Enter value: "))
+    d[key] = value
+my_dict = { 'apple': 50, 'banana': 20, 'cherry': 40 }
