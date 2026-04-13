12d7a8d52bad813da95871d52d65e774fb26209d
Vignapriya <vigna1419@gmail.com>
2026-04-13 11:55:29 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,8 @@
+def maximum(a,b,c):
+    if(a>b)and(a>c):
+        print(a," is greatest")
+    elif(b>c):
+        print(b," is greatest")
+    else:
+        print(c," is greatest")
+maximum(10,5,20)
